df60f67b3aa9e8a0db3221e9aad3f37b3b1047bb
ASazhin97 <alexandra@sazhin.us>
2020-03-26 15:33:06 -0400

    routing set up and basic login page created

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { LoginPageComponent } from './login-page/login-page.component'
 
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path:  '', redirectTo:  'login', pathMatch:  'full' },
+  { path: 'login', component: LoginPageComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
