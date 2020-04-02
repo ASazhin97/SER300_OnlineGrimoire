d4f763bc37cfe79ebd47154622ab44c82a10685c
ASazhin97 <alexandra@sazhin.us>
2020-04-02 17:03:11 -0400

    basic side nav

--- src/app/main-dash/side-nav/side-nav.component.ts
+++ src/app/main-dash/side-nav/side-nav.component.ts
@@ -6,10 +6,13 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./side-nav.component.css']
 })
 export class SideNavComponent implements OnInit {
-
+  NoteFiles = [];
   constructor() { }
 
   ngOnInit(): void {
+    //get files from database, but using just some stuff to make it look okay right now
+    this.NoteFiles.push({Name:"Witcher", HoursPlayed:64, Notes: "Looking for Yennefer"});
+
   }
 
 }
