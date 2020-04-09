8cc63b1f120699bb90945c2aaa7d6c7c9bc6ea1f
ASazhin97 <alexandra@sazhin.us>
2020-04-08 22:17:11 -0400

    put request added for update games

--- backend/dashboard.js
+++ backend/dashboard.js
@@ -39,6 +39,39 @@ function createRouter(db, dbname){
         )
     })
 
+    router.put('/update', function(req, res, next) {
+        token = req.query.t;
+        id = req.query.id;
+        field = req.query.f;
+        data = req.query.d;
+
+        if(field != "HoursPlayed" && field != "receiveNews"){
+            db.query(
+                'UPDATE '+dbname+'.'+token+' SET '+field+' = \"'+data+'\" WHERE id = '+id,
+                (error) => {
+                    if(error){
+                        console.log(error);
+                        res.status(500).json({status:'error'});
+                    } else {
+                        res.status(200).json({status: 'ok'});
+                    }
+                }
+            )
+        } else {
+            db.query(
+                'UPDATE '+dbname+'.'+token+' SET '+field+' = '+data+' WHERE id = '+id,
+                (error) => {
+                    if(error){
+                        console.log(error);
+                        res.status(500).json({status:'error'});
+                    } else {
+                        res.status(200).json({status: 'ok'});
+                    }
+                }
+            )
+        }
+    })
+
     return router;
 }
 
